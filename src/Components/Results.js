bfab0dc1dc8a7580f40ccbc8c87a25a274a01d6e
mnai01 <Imatlak03@gmail.com>
2020-03-02 22:25:19 -0500

    Added styling and changed css file name

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Result from "./Result";
+import classes from "./Results.module.css";
 
 const Results = props => {
   console.log(props.results);
   return (
-    <div className="results">
+    <div className={classes.results}>
       <ul>
         {props.results.map(result => (
           <Result className="result" key={result.id} result={result} />
