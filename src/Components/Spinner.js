c7e3c9f18ab22f4a404eb10aadd2baee7ab45711
mnai01 <imatlak03@gmail.com>
2020-03-03 02:55:59 -0500

    Added load spinner

---
+++ src/Components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SpinnerIcon from "./SpinnerIcon";
+
+const Spinner = () => {
+  return (
+    <div className="Spinner">
+      <SpinnerIcon />
+    </div>
+  );
+};
+
+export default Spinner;
