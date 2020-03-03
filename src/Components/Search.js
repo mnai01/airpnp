8627ae7aa4cf6176fed68bd2f9843d4d3d410bd0
mnai01 <imatlak03@gmail.com>
2020-03-03 02:55:38 -0500

    Cleaned up and added load handler

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,17 +2,16 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import classes from "./Search.module.css";
 
+const publicBathroomURL =
+  "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=100&offset=0&ada=false&unisex=false";
+const searchLocationURL =
+  "https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=";
+
 const Search = props => {
-  const publicBathroomURL =
-    "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=100&offset=0&ada=false&unisex=false";
-  const searchLocationURL =
-    "https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=";
-  let i = 0;
-  const search = e => {
+  const fetchData = e => {
     let value = e.target.value;
     if (e.key === "Enter") {
       props.changeMarker(null);
-      console.log(value);
       axios.get(searchLocationURL + value).then(res => {
         let newResults = res.data.records;
         newResults.map(results => {
@@ -20,13 +19,11 @@ const Search = props => {
             let lat = results.fields.latitude;
             let lng = results.fields.longitude;
             props.changeCord(lng, lat);
-            // setLat(results.fields.latitude);
-            // setLng(results.fields.longitude);
-            console.log(results.fields.city);
-            console.log(
-              results.fields.longitude + " " + results.fields.latitude
-            );
+            // I dont know if this needs to be here? but it fixes the array-callback-return warning
+            return true;
           }
+          // I dont know if this needs to be here? but it fixes the array-callback-return warning
+          return false;
         });
       });
     }
@@ -52,6 +49,8 @@ const Search = props => {
   };
 
   useEffect(() => {
+    props.changeLoad(true);
+
     axios
       .get(
         publicBathroomURL +
@@ -61,17 +60,9 @@ const Search = props => {
           props.state.currentLng
       )
       .then(data => {
+        props.changeLoad(false);
         let newResults = data.data;
-        console.log(newResults);
         props.changeBath(newResults);
-        console.log(
-          "URL CALLED: " +
-            publicBathroomURL +
-            "&lat=" +
-            props.state.currentLat +
-            "&lng=" +
-            props.state.currentLng
-        );
       })
       .catch(err => {
         console.log(err);
@@ -85,7 +76,7 @@ const Search = props => {
         className={classes.searchBox}
         type="text"
         placeholder="Enter Zip Code"
-        onKeyPress={search}
+        onKeyPress={fetchData}
       ></input>
     </div>
   );
