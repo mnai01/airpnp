a09c9c5d93cee6e6a36014d00891e8d2d7f3317e
mnai01 <imatlak03@gmail.com>
2020-03-28 03:32:50 -0400

    Cleaned up unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from "react";
 import classes from "./App.module.css";
-import SearchPage from "./Components/SearchPage/SearchPage";
-import Search from "./Components/Search/Search";
-import Results from "./Components/Results//Results";
-import MapContainer from "./Components/MapContainer/MapContainer";
-import Spinner from "./Components/Spinner/Spinner";
-import MainMenuMap from "./Components/MainMenuMap/MainMenuMap";
 import Content from "./Content";
 
 function App() {
