3612865677ecc0671e846f3cf05db248017c8546
mnai01 <imatlak03@gmail.com>
2020-03-28 03:33:52 -0400

    Removed unused imports/code

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import SearchPage from "./Components/SearchPage/SearchPage";
-import HomePage from "./Components/HomePage/HomePage.";
-import About from "./Components/CareerPath/CareerPath";
 import ToolBar from "./Components/ToolBar/Toolbar";
 import SideDrawer from "./Components/SideDrawer/SideDrawer";
 import Backdrop from "./Components/Backdrop/Backdrop";
-import CareerPath from "./Components/CareerPath/CareerPath";
-import GeoLocation from "./Components/GeoLocation/GeoLocation";
+
 import Aux from "./hoc/auxHOC/auxHOC";
 
 const Content = () => {
@@ -29,14 +24,12 @@ const Content = () => {
   }
   return (
     <Aux>
-      <Router>
-        <ToolBar drawerClickHandler={drawerToggleClickHandler} />
-        <SideDrawer show={sideDraw} />
-        {backdrop}
-        {/* A <Switch> looks through its children <Route>s and
+      <ToolBar drawerClickHandler={drawerToggleClickHandler} />
+      <SideDrawer show={sideDraw} />
+      {backdrop}
+      {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
-        {/* Did not do 41:11 UI nav video */}
-      </Router>
+      {/* Did not do 41:11 UI nav video */}
     </Aux>
   );
 };
