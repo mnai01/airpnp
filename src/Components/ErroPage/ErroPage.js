96a844cff680da22af80d4bfa0b6a74aee9c7867
mnai01 <imatlak03@gmail.com>
2020-03-28 04:02:41 -0400

    404 Error page added

--- src/Components/ErroPage/ErroPage.js
+++ src/Components/ErroPage/ErroPage.js
@@ -1,6 +1,12 @@
 import React from "react";
+import classes from "./ErroPage.module.css";
+import ErrorIMG from "../../assets/404error.jpg";
 const ErroPage = () => {
-  return <p>404 not found</p>;
+  return (
+    <div className={classes.container}>
+      <img src={ErrorIMG}></img>
+    </div>
+  );
 };
 
 export default ErroPage;
