0a9d4c20293dd0ce49b1d6cf7a01c19fa4f1be33
mnai01 <imatlak03@gmail.com>
2020-04-12 23:52:14 -0400

    Added custom popup

--- src/Components/MapContainer/MapContainer.js
+++ src/Components/MapContainer/MapContainer.js
@@ -4,8 +4,11 @@ import L from "leaflet";
 import Toilet from "../../assets/Toiletv1.png";
 import ToiletShadow from "../../assets/Toilet-Shadow.png";
 import classes from "./MapContainer.module.css";
+import "./MapContainer.css";
 
-const MapContainer = props => {
+import { popupContent, popupHead, popupText, okText } from "./popupStyles";
+
+const MapContainer = (props) => {
   var toiletIcon = L.icon({
     iconUrl: Toilet,
     shadowUrl: ToiletShadow,
@@ -13,7 +16,7 @@ const MapContainer = props => {
     shadowSize: [50, 75], // size of the shadow
     iconAnchor: [22, 40], // point of the icon which will correspond to marker's location
     shadowAnchor: [23, 40], // the same for the shadow
-    popupAnchor: [0, -30]
+    popupAnchor: [0, -30],
   });
 
   return (
@@ -30,7 +33,7 @@ const MapContainer = props => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {props.state.privateResults.map(result => (
+      {props.state.privateResults.map((result) => (
         <Marker
           // icon={toiletIcon}
           key={result.id}
@@ -38,7 +41,7 @@ const MapContainer = props => {
         />
       ))}
 
-      {props.state.results.map(result => (
+      {props.state.results.map((result) => (
         <Marker
           icon={toiletIcon}
           key={result.id}
@@ -50,9 +53,10 @@ const MapContainer = props => {
       ))}
       {props.state.markerPopup && (
         <Popup
+          className="custom-popup"
           position={[
             props.state.markerPopup.latitude,
-            props.state.markerPopup.longitude
+            props.state.markerPopup.longitude,
           ]}
           onClose={() => {
             props.marker(null);
@@ -60,11 +64,13 @@ const MapContainer = props => {
         >
           <div>
             <h2>{props.state.markerPopup.name}</h2>
-            <p>
-              {props.state.markerPopup.street},{props.state.markerPopup.city},
-              {props.state.markerPopup.state}
-            </p>
-            <p>{props.state.markerPopup.comment}</p>
+            <div className="info-container">
+              <p className="address">
+                {props.state.markerPopup.street},{props.state.markerPopup.city},
+                {props.state.markerPopup.state}
+              </p>
+              <p className="comment">{props.state.markerPopup.comment}</p>
+            </div>
           </div>
         </Popup>
       )}
