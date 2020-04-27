bf0826fbe7cc7767971a22a8cc75b1b5505b645e
BernsteinBearrr <andretbernstein@gmail.com>
2020-04-26 21:48:59 -0400

    User Info page formatting

--- src/Components/UserInfoPage/UserInfoPage.js
+++ src/Components/UserInfoPage/UserInfoPage.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
 import Aux from "../../hoc/auxHOC/auxHOC";
-
 import classes from "./UserInfoPage.module.css";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 const url =
   "https://cors-anywhere.herokuapp.com/https://www.airpnpbcs430w.info/User/API/getuser/";
@@ -85,39 +85,76 @@ const UserInfoPage = (props) => {
 
   return (
     <Aux>
-      <div>
-        <h1>
-          {Cookies.get("Username")} ({totalScore})
-        </h1>
-        {userInfo ? (
-          <Aux>
-            <div>
-              <p>First name: {userInfo.first_name}</p>
-              <p>Last name: {userInfo.last_name}</p>
-              <p>Address: {userInfo.home_address}</p>
-              <p>Address: {userInfo.personalEmail}</p>
-              {bathrooms ? (
-                <div>
-                  {bathrooms.map((res) => {
-                    return (
-                      <p>
-                        {res.address_id.address_line1} {res.address_id.city}{" "}
-                        {res.address_id.state} {res.address_id.zip}
-                      </p>
-                    );
-                  })}
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-            <div></div>
-          </Aux>
-        ) : (
-          <div>
-            <h1>Loading...</h1>
+      <div className={classes.userInfoDiv}>
+        <div className={classes.userWrap}>
+          <div className={classes.imgRounded}>
+            <img src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
           </div>
-        )}
+          <h2>
+            {Cookies.get("Username")} ({totalScore})
+          </h2>
+          {userInfo ? (
+            <Aux>
+              <div>
+                <p>First name: {userInfo.first_name}</p>
+                <p>Last name: {userInfo.last_name}</p>
+                <p>Address: {userInfo.home_address}</p>
+                <p>Address: {userInfo.personalEmail}</p>
+                {bathrooms ? (
+                  <div>
+                    {bathrooms.map((res) => {
+                      return (
+                        <p>
+                          {res.address_id.address_line1} {res.address_id.city}{" "}
+                          {res.address_id.state} {res.address_id.zip}
+                        </p>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div></div>
+            </Aux>
+          ) : (
+            <div>
+              <h1>Loading...</h1>
+            </div>
+          )}
+        </div>
+        <div className={classes.formWrap}>
+          <div>
+            <h5>Update User Information</h5>
+          </div>
+          <Form>
+            <FormGroup>
+              <Label for="firstname">First name</Label>
+              <Input type="text" name="firstname" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="lastname">Last name</Label>
+              <Input type="text" name="lastname" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="address">Street Address</Label>
+              <Input type="text" name="address" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="zip">Zip Code</Label>
+              <Input type="text" name="zip" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email">Email Address</Label>
+              <Input type="email" name="email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="phone">Primery Phone Number</Label>
+              <Input type="number" name="phone" />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </div>
       </div>
     </Aux>
   );
