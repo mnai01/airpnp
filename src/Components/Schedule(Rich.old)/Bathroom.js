732fd2874eae408ca495d1050c207375540ce8d1
mnai01 <imatlak03@gmail.com>
2020-04-30 18:30:53 -0400

    Added pick address

--- src/Components/Schedule(Rich.old)/Bathroom.js
+++ src/Components/Schedule(Rich.old)/Bathroom.js
@@ -6,15 +6,42 @@ import {
   Label,
   Input,
   FormText,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
   Row,
 } from "reactstrap";
 import axios from "axios";
 import Aux from "../../hoc/auxHOC/auxHOC";
-
+import PlacesAutocomplete from "react-places-autocomplete";
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from "react-places-autocomplete";
 import classes from "./Bathroom.module.css";
+
+const URL = "https://www.airpnpbcs430w.info/Bathrooms/Ratings/API";
 
 const Bathroom = (props) => {
   const [file, setFile] = useState();
+  const [address, setAddress] = useState("");
+  const [modal, setModal] = useState(false);
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null,
+  });
+
+  const handleToggleModale = () => setModal(!modal);
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latlng = await getLatLng(results[0]);
+    setAddress(value);
+    setCoordinates(latlng);
+    console.log("lat: " + latlng.lat + " " + "lng: " + latlng.lng);
+  };
 
   const handlerFileSelected = (event) => {
     setFile(event.target.files[0]);
@@ -22,24 +49,41 @@ const Bathroom = (props) => {
   };
 
   const handleFileUpload = () => {
-    // const fd = new FormData();
-    // fd.append("image", file, file.name);
-    // axios
-    //   .post(url, fd, {
-    //     onUploadProgress: (progressEvent) => {
-    //       console.log(
-    //         "Upload Progress" +
-    //           (progressEvent.loaded / progressEvent.total) * 100 +
-    //           "%"
-    //       );
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    const fd = new FormData();
+    fd.append("image", file, file.name);
+    fd.append("data", data);
+    let data = {
+      title: "title",
+      tagline: "tagline",
+      slug: "slug",
+      body: "body",
+    };
+
+    axios
+      .post(
+        URL,
+        fd,
+        {
+          onUploadProgress: (progressEvent) => {
+            console.log(
+              "Upload Progress" +
+                (progressEvent.loaded / progressEvent.total) * 100 +
+                "%"
+            );
+          },
+        },
+        {
+          headers: {
+            "content-type": "multipart/form-data",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <Aux>
@@ -51,12 +95,126 @@ const Bathroom = (props) => {
             </FormGroup>
 
             <FormGroup>
-              <Label for="Address">Address</Label>
+              <Label for="AmountofBathrooms">Pick an address</Label>
               <Input
-                type="Address"
-                placeholder="Enter Bathroom address"
-                required
-              />
+                type="select"
+                name="selectMultiple"
+                id="multipleBathrooms"
+                mulitple
+              >
+                <option>Address 1</option>
+                <option>Address 2</option>
+                <option>Address 3</option>
+                <option>Address 4</option>
+              </Input>
+            </FormGroup>
+
+            <FormGroup>
+              <Button onClick={handleToggleModale}>Add an address</Button>
+            </FormGroup>
+
+            <Modal isOpen={modal} toggle={handleToggleModale}>
+              <ModalHeader toggle={handleToggleModale}>
+                Address Adding
+              </ModalHeader>
+              <ModalBody>
+                <FormGroup>
+                  <Label for="Address">Address</Label>
+
+                  <PlacesAutocomplete
+                    value={address}
+                    onChange={setAddress}
+                    onSelect={handleSelect}
+                  >
+                    {({
+                      getInputProps,
+                      suggestions,
+                      getSuggestionItemProps,
+                      loading,
+                    }) => (
+                      <div className={classes.searchBox}>
+                        <Input
+                          {...getInputProps({ placeholder: "Type address" })}
+                        />
+                        <div>{loading ? <div>...loading</div> : null}</div>
+                        {suggestions.map((suggestion) => {
+                          const style = {
+                            backgroundColor: suggestion.active
+                              ? "#41b6e6"
+                              : "#fff",
+                          };
+                          return (
+                            <div
+                              {...getSuggestionItemProps(suggestion, { style })}
+                            >
+                              {suggestion.description}
+                              {console.log(suggestion)}
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </PlacesAutocomplete>
+                </FormGroup>
+
+                <FormGroup>
+                  <Label>ZIP</Label>
+                  <Input type="number" placeholder="ZIP" requried />
+                </FormGroup>
+
+                <FormGroup>
+                  <Label>City</Label>
+                  <Input type="text" placeholder="City" required />
+                </FormGroup>
+
+                <FormGroup>
+                  <Label>State</Label>
+                  <Input type="text" placeholder="State" required></Input>
+                </FormGroup>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={handleToggleModale}>
+                  Do Something
+                </Button>
+                <Button color="secondary" onClick={handleToggleModale}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Modal>
+
+            {/* <FormGroup>
+              <Label for="Address">Address</Label>
+
+              <PlacesAutocomplete
+                value={address}
+                onChange={setAddress}
+                onSelect={handleSelect}
+              >
+                {({
+                  getInputProps,
+                  suggestions,
+                  getSuggestionItemProps,
+                  loading,
+                }) => (
+                  <div className={classes.searchBox}>
+                    <Input
+                      {...getInputProps({ placeholder: "Type address" })}
+                    />
+                    <div>{loading ? <div>...loading</div> : null}</div>
+                    {suggestions.map((suggestion) => {
+                      const style = {
+                        backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
+                      };
+                      return (
+                        <div {...getSuggestionItemProps(suggestion, { style })}>
+                          {suggestion.description}
+                          {console.log(suggestion)}
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </PlacesAutocomplete>
             </FormGroup>
 
             <FormGroup>
@@ -72,7 +230,7 @@ const Bathroom = (props) => {
             <FormGroup>
               <Label>State</Label>
               <Input type="text" placeholder="State" required></Input>
-            </FormGroup>
+            </FormGroup> */}
 
             <FormGroup>
               <Label for="BathroomFeatures">
