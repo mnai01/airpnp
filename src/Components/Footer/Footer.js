b4b25c16c9cc4c5be21655a7d4a578dac6386230
mnai01 <imatlak03@gmail.com>
2020-05-04 06:53:49 -0400

    Added footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import classes from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.about}>
+        <h2>Airpnp</h2>
+        <p>
+          Airpnp is an app which allowed you to find public/private bathrooms
+        </p>
+      </div>
+      <div className={classes.repos}>
+        <h3>Who Are We?</h3>
+        <ul>
+          <li>
+            <a href="https://github.com/mnai01">Ian Matlak</a>, Mobile + Web App
+            &amp; Frontend
+          </li>
+          <li>
+            <a href="https://github.com/Brandon Berke">Brandon</a>,Backend API
+          </li>
+          <li>
+            <a href="https://github.com/BernsteinBearrr">Andre</a>, Frontend
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">Benny</a>, Documentation
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">Rich</a>, Documentation
+          </li>
+        </ul>
+      </div>
+      <div className={classes.copy}>
+        <p>Copyright &copy; 2020</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
